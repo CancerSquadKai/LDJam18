c8262f0413b990afebd9b5e151010441229fac79
MrMesk <mesk.utp@gmail.com>
2018-12-03 12:38:12 +0100

    SFX, Statues, Test scene for Vincent

---
+++ LDJam18/Assets/Scripts/EnemyKillDetection.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyKillDetection : MonoBehaviour
+{
+	public RoomController roomController;
+	// Use this for initialization
+
+	private void OnDestroy ()
+	{
+		roomController.RemoveEnemy(this);
+	}
+}
