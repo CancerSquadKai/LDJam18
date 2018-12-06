c0e27a9663e876d1778cf5cb9d5f0f5a8b35a148
MrMesk <mesk.utp@gmail.com>
2018-12-06 14:50:22 +0100

    Menu Pause + quelques tweaks

---
+++ LDJam18/Assets/Scripts/PauseManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseManager : MonoBehaviour
+{
+	public GameObject pausePanel;
+
+	bool isPaused = false;
+	// Use this for initialization
+	
+	public void Continue()
+	{
+		isPaused = false;
+		Time.timeScale = 1f;
+		pausePanel.SetActive(false);
+	}
+
+	public void Quit ()
+	{
+		Application.Quit();
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		if(Input.GetKeyDown(KeyCode.Escape))
+		{
+			pausePanel.SetActive(!isPaused);
+
+			if(!isPaused)
+			{
+				Time.timeScale = 0.01f;
+			}
+			else
+			{
+				Time.timeScale = 1f;
+			}
+
+			isPaused = !isPaused;
+		}
+	}
+}
